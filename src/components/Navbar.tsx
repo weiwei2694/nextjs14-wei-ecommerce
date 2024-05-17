8b10c777a37c37413da2a7ca3b75f45aeee751c6
Arya Priema <99aryaprima@gmail.com>
2024-05-17 23:28:50 +0700

    feat: Navbar - Mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,29 @@
 'use client';
 
 import React from 'react';
-import { Button, buttonVariants } from './ui/button';
-import { ArrowRightIcon, WalletCards } from 'lucide-react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { ArrowRightIcon, Menu, WalletCards } from 'lucide-react';
+
+import { cn } from '@/lib/utils';
+
 import { LogoutLink, LoginLink } from '@kinde-oss/kinde-auth-nextjs/components';
 import type { KindeUser } from '@kinde-oss/kinde-auth-nextjs/types';
 
+import { Button, buttonVariants } from './ui/button';
+import {
+	Sheet,
+	SheetClose,
+	SheetContent,
+	SheetDescription,
+	SheetFooter,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from '@/components/ui/sheet';
+
 import { Inter } from 'next/font/google';
-import { cn } from '@/lib/utils';
-import { usePathname } from 'next/navigation';
 const inter = Inter({
 	subsets: ['latin'],
 });
@@ -34,7 +48,7 @@ const Navbar = ({ user }: { user: KindeUser | null }) => {
 	const pathname = usePathname();
 
 	return (
-		<nav className='sticky z-[100] top-0 bg-white/50 backdrop-blur-lg transition-all border-b border-gray-200'>
+		<nav className='sticky z-[100] top-0 bg-white border-b border-gray-200'>
 			<div className='max-w-7xl mx-auto'>
 				<div
 					className={cn(
@@ -50,7 +64,7 @@ const Navbar = ({ user }: { user: KindeUser | null }) => {
 							STORE
 						</Link>
 
-						<ul className='flex items-center space-x-8'>
+						<ul className='hidden lg:flex items-center space-x-8'>
 							{lists.map((list) => (
 								<li key={list.name}>
 									<Link
@@ -74,28 +88,88 @@ const Navbar = ({ user }: { user: KindeUser | null }) => {
 							<WalletCards className='w-5 h-5 mr-1.5' />
 							<span className='font-bold text-xs'>12</span>
 						</Button>
-						<div className='h-10 w-px bg-zinc-200 hidden sm:block' />
-						{!user ? (
-							<LoginLink
-								className={buttonVariants({
-									size: 'sm',
-									variant: 'ghost',
-								})}
-								postLoginRedirectURL='/'
-							>
-								Sign In <ArrowRightIcon className='ml-1.5 h-5 w-5' />
-							</LoginLink>
-						) : (
-							<LogoutLink
-								className={buttonVariants({
-									size: 'sm',
-									variant: 'ghost',
-								})}
-							>
-								Log Out
-								<ArrowRightIcon className='ml-1.5 h-5 w-5' />
-							</LogoutLink>
-						)}
+
+						<div className='h-10 w-px bg-zinc-200 hidden lg:block' />
+
+						<div
+							aria-hidden='true'
+							className='hidden lg:block'
+						>
+							{!user ? (
+								<LoginLink
+									className={buttonVariants({
+										size: 'sm',
+										variant: 'ghost',
+									})}
+									postLoginRedirectURL='/'
+								>
+									Sign In <ArrowRightIcon className='ml-1.5 h-5 w-5' />
+								</LoginLink>
+							) : (
+								<LogoutLink
+									className={buttonVariants({
+										size: 'sm',
+										variant: 'ghost',
+									})}
+								>
+									Log Out
+									<ArrowRightIcon className='ml-1.5 h-5 w-5' />
+								</LogoutLink>
+							)}
+						</div>
+
+						<div
+							aria-hidden='true'
+							className='lg:hidden'
+						>
+							<Sheet>
+								<SheetTrigger
+									className={buttonVariants({
+										size: 'sm',
+										variant: 'ghost',
+									})}
+								>
+									<Menu className='w-5 h-5' />
+								</SheetTrigger>
+								<SheetContent
+									side='top'
+									className={cn(inter.className, 'flex flex-col space-y-4')}
+								>
+									<SheetHeader>
+										<ul className='flex flex-col items-start space-y-4'>
+											{lists.map((list) => (
+												<li key={list.name}>
+													<Link
+														href={list.href}
+														className={cn(
+															'text-zinc-900 tracking-tight text-sm ',
+															pathname === list.href
+																? 'font-semibold'
+																: 'font-normal'
+														)}
+													>
+														{list.name}
+													</Link>
+												</li>
+											))}
+										</ul>
+									</SheetHeader>
+									<SheetFooter className='!mt-0 !pt-0'>
+										<LogoutLink
+											className={buttonVariants({
+												size: 'default',
+												variant: 'default',
+												className:
+													'bg-zinc-900 hover:bg-zinc-900/90 transition-all duration-200 w-full',
+											})}
+										>
+											Log Out
+											<ArrowRightIcon className='ml-1.5 h-5 w-5' />
+										</LogoutLink>
+									</SheetFooter>
+								</SheetContent>
+							</Sheet>
+						</div>
 					</div>
 				</div>
 			</div>
