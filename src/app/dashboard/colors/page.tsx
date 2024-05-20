36c11a6bee72ad5db6ab7826baae1ce5d5b3c112
Arya Priema <99aryaprima@gmail.com>
2024-05-20 21:35:30 +0700

    feat: /dashboard/colors/page.tsx - added HeadSection

--- src/app/dashboard/colors/page.tsx
+++ src/app/dashboard/colors/page.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import BodySection from '@/components/dashboard/BodySection';
+import HeadSection from '@/components/dashboard/HeadSection';
+
 const Page = () => {
-	return <div>Page</div>;
+	return (
+		<BodySection>
+			<HeadSection
+				title='Colors(3)'
+				subtitle='Manage colors for your product'
+				isNewButtonVisible
+				newButtonPath='/dashboard/colors/create'
+			/>
+		</BodySection>
+	);
 };
 
 export default Page;
