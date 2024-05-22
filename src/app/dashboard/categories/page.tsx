df7e740acd2feb8ef2fab0e3376f3e901dc57d55
Arya Priema <99aryaprima@gmail.com>
2024-05-22 21:01:42 +0700

    feat: /dashboard/categories/page - added HeadSection

--- src/app/dashboard/categories/page.tsx
+++ src/app/dashboard/categories/page.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { redirect } from 'next/navigation';
 
-const Page = () => {
-	return <div>Page</div>;
+import BodySection from '@/components/dashboard/BodySection';
+import HeadSection from '@/components/dashboard/HeadSection';
+
+const Page = ({
+	searchParams,
+}: {
+	searchParams: {
+		[key: string]: string | string[] | undefined;
+	};
+}) => {
+	const { page } = searchParams;
+	if (!page || typeof page !== 'string') {
+		redirect('/dashboard/categories?page=1');
+	}
+
+	return (
+		<BodySection>
+			<HeadSection
+				title={`Categories(0)`}
+				subtitle='Manage categories for your product'
+				isNewButtonVisible
+				newButtonPath='/dashboard/categories/create'
+			/>
+		</BodySection>
+	);
 };
 
 export default Page;
