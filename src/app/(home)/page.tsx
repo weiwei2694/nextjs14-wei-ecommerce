bb57ca4409b7e507e45d191e5de2d48d8000ff00
Wei <99aryaprima@gmail.com>
2024-05-28 19:31:46 +0700

    feat: / - featured products

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,49 @@
 import React from 'react';
 
-const Page = () => {
-	return <div>Page</div>;
+import { db } from '@/db';
+
+import CardProduct from '@/components/home/CardProduct';
+
+import type { Image, Product } from '@prisma/client';
+
+export type ProductFeatured = {
+	images: Image[];
+	category: {
+		name: string;
+	};
+} & Product;
+
+const Page = async () => {
+	const products: ProductFeatured[] = await db.product.findMany({
+		where: {
+			isArchived: false,
+			isFeatured: true,
+			images: {
+				some: {},
+			},
+		},
+		include: {
+			images: true,
+			category: {
+				select: {
+					name: true,
+				},
+			},
+		},
+	});
+
+	return (
+		<div className='max-w-7xl mx-auto my-8 px-6 2xl:px-0'>
+			<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+				{products.map((product, productIndex) => (
+					<CardProduct
+						key={productIndex}
+						product={product}
+					/>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Page;
---
+++ src/components/home/CardProduct.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Recursive } from 'next/font/google';
+
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+
+import { cn, formatPrice } from '@/lib/utils';
+
+import type { ProductFeatured } from '@/app/(home)/page';
+
+const recursive = Recursive({ subsets: ['latin'] });
+
+const CardProduct = ({ product }: { product: ProductFeatured }) => {
+	return (
+		<Card className='group'>
+			<CardHeader>
+				<div className='relative'>
+					<Image
+						src={product.images[0].url}
+						alt='product img'
+						width={300}
+						height={260}
+						className='w-full h-[260px] object-cover rounded-lg'
+						priority
+					/>
+
+					<div className='absolute hidden inset-0 group-hover:lg:flex items-end justify-center pb-5 px-5'>
+						<Button
+							className={cn(
+								recursive.className,
+								'tracking-[.1rem] rounded-none w-full'
+							)}
+						>
+							ADD TO CART
+						</Button>
+					</div>
+				</div>
+			</CardHeader>
+			<CardContent>
+				<Link href='/'>
+					<CardTitle className='text-xl'>{product.title}</CardTitle>
+				</Link>
+				<CardDescription className='capitalize'>
+					{product.category.name}
+				</CardDescription>
+			</CardContent>
+			<CardFooter>
+				<CardTitle className='text-lg'>{formatPrice(product.price)}</CardTitle>
+			</CardFooter>
+		</Card>
+	);
+};
+
+export default CardProduct;
