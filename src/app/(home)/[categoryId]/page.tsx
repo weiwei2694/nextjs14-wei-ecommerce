6821b72eb42c7f6024bdf18062625e3dd9e8b0c3
Wei <99aryaprima@gmail.com>
2024-05-28 20:03:38 +0700

    feat: (home)/[categoryId] - products

--- src/app/(home)/[categoryId]/page.tsx
+++ src/app/(home)/[categoryId]/page.tsx
@@ -4,12 +4,29 @@ import { notFound } from 'next/navigation';
 import { db } from '@/db';
 
 import Billboard from '@/components/home/Billboard';
+import CardProduct from '@/components/home/CardProduct';
 
 const Page = async ({ params }: { params: { categoryId: string } }) => {
 	const { categoryId } = params;
 
 	const category = await db.category.findUnique({
 		where: { id: categoryId },
+		include: {
+			products: {
+				where: {
+					isArchived: false,
+					images: { some: {} },
+				},
+				include: {
+					images: true,
+					category: {
+						select: {
+							name: true,
+						},
+					},
+				},
+			},
+		},
 	});
 	if (!category) {
 		return notFound();
@@ -21,6 +38,14 @@ const Page = async ({ params }: { params: { categoryId: string } }) => {
 				title={category.title}
 				img={category.billboard}
 			/>
+			<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+				{category.products.map((product, productIndex) => (
+					<CardProduct
+						key={productIndex}
+						product={product}
+					/>
+				))}
+			</div>
 		</div>
 	);
 };
