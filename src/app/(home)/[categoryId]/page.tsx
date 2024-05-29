1bd5837ba989f0b7bd35e4688f09a94c1c91c2fc
Wei <99aryaprima@gmail.com>
2024-05-29 20:04:27 +0700

    feat: (home)/[categoryId] - filter mobile-desktop

--- src/app/(home)/[categoryId]/page.tsx
+++ src/app/(home)/[categoryId]/page.tsx
@@ -9,6 +9,17 @@ import { buildRedirectUrl } from '@/lib/utils';
 import Billboard from '@/components/home/Billboard';
 import CardProduct from '@/components/home/CardProduct';
 import { buttonVariants } from '@/components/ui/button';
+import {
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetFooter,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from '@/components/ui/sheet';
+
+import { SlidersHorizontal } from 'lucide-react';
 
 const Page = async ({
 	params,
@@ -62,8 +73,9 @@ const Page = async ({
 				title={category.title}
 				img={category.billboard}
 			/>
-			<div className='grid grid-cols-5 gap-8 mb-8'>
-				<div className='col-span-1 flex flex-col gap-8'>
+			<div className='grid grid-cols-5 gap-5 xl:gap-8 mb-8'>
+				{/* filter large desktop */}
+				<div className='col-span-1 hidden xl:flex flex-col gap-8'>
 					<div className='flex flex-col gap-2'>
 						<h2 className='font-semibold'>Colors</h2>
 						<hr />
@@ -125,7 +137,105 @@ const Page = async ({
 					</div>
 				</div>
 
-				<div className='col-span-4 flex-1 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5'>
+				{/* filter mobile - desktop */}
+				<div className='col-span-full block xl:hidden'>
+					<Sheet>
+						<SheetTrigger
+							className={buttonVariants({
+								variant: 'secondary',
+								size: 'sm',
+							})}
+						>
+							<SlidersHorizontal className='w-4 h-4' />
+						</SheetTrigger>
+						<SheetContent side='left'>
+							<SheetHeader className='mb-8 text-start'>
+								<SheetTitle>Filter</SheetTitle>
+								<SheetDescription>
+									Use filters to refine the results according to your
+									preferences.
+								</SheetDescription>
+							</SheetHeader>
+							<div className='flex flex-col gap-8'>
+								<div className='flex flex-col gap-2'>
+									<h2 className='font-semibold'>Colors</h2>
+									<hr />
+									<div className='flex flex-wrap gap-2'>
+										<Link
+											href={buildRedirectUrl(categoryId, undefined, sizeFilter)}
+											className={buttonVariants({
+												variant:
+													colorFilter === undefined ? 'default' : 'outline',
+											})}
+										>
+											All
+										</Link>
+										{colors.map((color, colorIndex) => (
+											<Link
+												href={buildRedirectUrl(
+													categoryId,
+													color.name,
+													sizeFilter
+												)}
+												key={colorIndex}
+												className={buttonVariants({
+													variant:
+														colorFilter?.toLowerCase() === color.name
+															? 'default'
+															: 'outline',
+													className: 'capitalize',
+												})}
+											>
+												{color.name}
+											</Link>
+										))}
+									</div>
+								</div>
+								<div className='flex flex-col gap-2'>
+									<h2 className='font-semibold'>Sizes</h2>
+									<hr />
+									<div className='flex flex-wrap gap-2'>
+										<Link
+											href={buildRedirectUrl(
+												categoryId,
+												colorFilter,
+												undefined
+											)}
+											className={buttonVariants({
+												variant:
+													sizeFilter === undefined ? 'default' : 'outline',
+												className: 'capitalize',
+											})}
+										>
+											All
+										</Link>
+										{sizes.map((size, sizeIndex) => (
+											<Link
+												href={buildRedirectUrl(
+													categoryId,
+													colorFilter,
+													size.value
+												)}
+												key={sizeIndex}
+												className={buttonVariants({
+													variant:
+														sizeFilter?.toUpperCase() === size.value
+															? 'default'
+															: 'outline',
+													className: 'capitalize',
+												})}
+											>
+												{size.name}
+											</Link>
+										))}
+									</div>
+								</div>
+							</div>
+						</SheetContent>
+					</Sheet>
+				</div>
+
+				<div className='col-span-full xl:col-span-4 flex-1 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5'>
 					{category.products.map((product, productIndex) => (
 						<CardProduct
 							key={productIndex}
