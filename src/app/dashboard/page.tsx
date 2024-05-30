0e99fa4bfe7f058cb53eaf2026ffa1268d8128d0
Wei <99aryaprima@gmail.com>
2024-05-30 21:37:41 +0700

    feat: /dashboard/page - show original data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 
 import BodySection from '@/components/dashboard/BodySection';
 import HeadSection from '@/components/dashboard/HeadSection';
-import {
-	Card,
-	CardDescription,
-	CardHeader,
-	CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 import { DollarSign, PackageSearch, Weight } from 'lucide-react';
 
-const Page = () => {
+import { formatPrice } from '@/lib/utils';
+
+import { db } from '@/db';
+
+const Page = async () => {
+	const activeListings = await db.product.count({
+		where: {
+			isArchived: false,
+			images: {
+				some: {},
+			},
+		},
+	});
+	const sales = await db.order.count({});
+
+	const orders = await db.order.findMany({});
+	const totalRevenue = orders.reduce((a, b) => a + b.total, 0);
+
 	return (
 		<BodySection>
 			<HeadSection
@@ -21,49 +33,37 @@ const Page = () => {
 
 			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
 				<Card>
-					<CardHeader>
-						<div className='flex flex-row justify-between items-start'>
-							<div className='flex flex-col gap-4'>
-								<CardTitle className='font-semibold text-lg'>
-									Total Revenue
-								</CardTitle>
-								<CardDescription className='text-zinc-900 font-extrabold text-2xl'>
-									$59.00
-								</CardDescription>
-							</div>
-							<DollarSign className='w-5 h-5 text-zinc-500' />
-						</div>
+					<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+						<CardTitle className='text-sm font-medium'>Total Revenue</CardTitle>
+						<DollarSign className='w-5 h-5 text-zinc-500' />
 					</CardHeader>
+					<CardContent>
+						<div className='text-2xl font-bold'>
+							{formatPrice(totalRevenue)}
+						</div>
+					</CardContent>
 				</Card>
 
 				<Card>
-					<CardHeader>
-						<div className='flex flex-row justify-between items-start'>
-							<div className='flex flex-col gap-4'>
-								<CardTitle className='font-semibold text-lg'>Sales</CardTitle>
-								<CardDescription className='text-zinc-900 font-extrabold text-2xl'>
-									+1
-								</CardDescription>
-							</div>
-							<Weight className='w-5 h-5 text-zinc-500' />
-						</div>
+					<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+						<CardTitle className='text-sm font-medium'>Sales</CardTitle>
+						<Weight className='w-5 h-5 text-zinc-500' />
 					</CardHeader>
+					<CardContent>
+						<div className='text-2xl font-bold'>+{sales}</div>
+					</CardContent>
 				</Card>
 
 				<Card>
-					<CardHeader>
-						<div className='flex flex-row justify-between items-start'>
-							<div className='flex flex-col gap-4'>
-								<CardTitle className='font-semibold text-lg'>
-									Products in Stock
-								</CardTitle>
-								<CardDescription className='text-zinc-900 font-extrabold text-2xl'>
-									7
-								</CardDescription>
-							</div>
-							<PackageSearch className='w-5 h-5 text-zinc-500' />
-						</div>
+					<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+						<CardTitle className='text-sm font-medium'>
+							Active Listings
+						</CardTitle>
+						<PackageSearch className='w-5 h-5 text-zinc-500' />
 					</CardHeader>
+					<CardContent>
+						<div className='text-2xl font-bold'>{activeListings}</div>
+					</CardContent>
 				</Card>
 			</div>
 		</BodySection>
